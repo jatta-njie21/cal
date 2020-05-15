a1519c3f83bde757dde03e2cf77d936733fa17b1
jatta-njie21 <laboyjatta@gmail.com>
2020-05-15 13:15:51 +0000

    Second Commit

--- test.py
+++ test.py
@@ -1,5 +1,41 @@
-def input():
+def addition():
 	pass
-	
+
+def subtraction():
+	pass
+
+def multiplication():
+	pass
+
+def division():
+	pass
+
 def modulos():
 	pass
+
+def inputs():
+	
+	num1 = float(input("NUM1 = "))
+	num2 = float(input("NUM1 = "))
+	operand = input("OPERAND =  ")
+
+	if operand == "+" or "plus" or "p" or "P" or "PLUS" or "Plus":
+		addition()
+
+	elif operand == "-" or "minus" or "m" or "M" or "MINUS" or "Minus":
+ 		subtraction()
+
+	elif operand ==  "*" or "times" or "t" or "T" or "TIMES" or "Times":
+		multiplication()
+
+	elif operand == "/" or "div" or "d" or "D" or "DIVIDE" or "Divide":
+		division()
+
+	elif operand == "%" or "mod" or "md" or "MD" or "MOD" or "Mod":
+		modulos()
+
+	else:
+		print("INVALID OPERAND!!!!!")
+
+while True:
+	inputs()
